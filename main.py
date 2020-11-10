2587c94ddb1d1e020d9ce7c87dcda7da933b5ce9
saileshkadam <saileshskadam@gmail.com>
2020-11-10 14:48:22 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import discord
-#print(discord.__version__)  # check to make sure at least once you're on the right version!
 
-token = "Nzc0MzYzNzM3NzcwNTU3NTAy.X6Wsbg.LZQoMY9izdfAZ5hVVjb4DowePvI"
+path = '/home/ubuntu/tokens/discordToken.txt'
+tokenFile = open(path,'r')
+token = tokenFile.read()
 
-text_channel_list = []
 client = discord.Client()  # starts the discord client.
 
 
@@ -16,7 +16,7 @@ async def on_ready():  # method expected by client. This runs once when connecte
 async def on_message(message):  # event that happens per any message.
 
     if message.content.startswith('!rant'):
-        await message.channel.send('Here is a random ramnbling of political nonsense from some website')
+        await message.channel.send('Here is a random rambling of political nonsense from some website')
 
 
 client.run(token)
