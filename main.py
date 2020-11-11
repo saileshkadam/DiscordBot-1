47dfdc404d665e3ccc37624280b4f0acdeaa30d3
saileshkadam <saileshskadam@gmail.com>
2020-11-11 09:00:18 -0600

    added conditionals

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import discord
 
 path = '/home/ubuntu/tokens/discordToken.txt'
-tokenFile = open(path,'r')
+tokenFile = open(path, 'r')
 token = tokenFile.read()
 
 client = discord.Client()  # starts the discord client.
@@ -16,18 +16,17 @@ async def on_ready():  # method expected by client. This runs once when connecte
 async def on_message(message):  # event that happens per any message.
 
     m_content = message.content.lower()
-    if message.content.startswith('!rant'):
-        await message.channel.send('Buckle up kiddos, its going to get political')
 
+    if m_content.startswith('!rant'):
+        await message.channel.send('Buckle up kiddos, its going to get political')
     if 'voting' in m_content or 'vote' in m_content:
         await message.add_reaction('<:soldiersam:774092343120887808>')
-    if ('tyrann' in m_content):
+    if 'tyranny' in m_content or 'tyrannical' in m_content or 'tyrant' in m_content:
         await message.add_reaction('<:nostep:773692274449580062>')
-    if ('tax' in m_content):
+    if 'tax' in m_content:
         await message.add_reaction('<:friedweegs:774055169569587260>')
     else:
         print()
-
 
 
 client.run(token)
