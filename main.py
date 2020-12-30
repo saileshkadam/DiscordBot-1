2f964ce3bc115b0143835dfd8444f5d74066b794
saileshkadam <saileshskadam@gmail.com>
2020-12-30 15:13:56 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,6 @@ import discord
 import praw
 import configparser
 
-#config_path = '/home/ubuntu/tokens/discordToken.txt'
-#tokenFile = open(discord_path, 'r')
-#token = tokenFile.read()
-#praw_path = '/home/ubuntu/tokens/praw_config.ini'
-
 parser = configparser.ConfigParser()
 
 config_path = '/home/ubuntu/tokens/keeg_config.ini'
@@ -31,11 +26,11 @@ async def on_message(message):  # event that happens per any message.
 
     if m_content.startswith('!rant'):
         await message.channel.send('Buckle up kiddos, its going to get political')
-    if 'voting' in m_content or 'vote' in m_content:
+    if ' voting ' in m_content or ' vote ' in m_content:
         await message.add_reaction('<:soldiersam:774092343120887808>')
-    if 'tyranny' in m_content or 'tyrannical' in m_content or 'tyrant' in m_content:
+    if ' tyranny ' in m_content or ' tyrannical ' in m_content or ' tyrant ' in m_content:
         await message.add_reaction('<:nostep:773692274449580062>')
-    if 'tax' in m_content:
+    if ' tax' in m_content:
         await message.add_reaction('<:friedweegs:774055169569587260>')
     if m_content.startswith('!kmeme'):
         subreddit = reddit.subreddit("libertarianmeme")
