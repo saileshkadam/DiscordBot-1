08977dfdb9d1d0ba016b905de6ecc50d6643430d
saileshkadam <saileshskadam@gmail.com>
2020-12-30 15:41:03 -0600

    Update main.py
    added meme validity check

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import praw
 import configparser
 
 parser = configparser.ConfigParser()
+lastTenPosts = [0];
 
 config_path = '/home/ubuntu/tokens/keeg_config.ini'
 parser.read(config_path)
@@ -13,6 +14,12 @@ reddit = praw.Reddit(client_id=parser.get('praw', 'client_id'),
                      user_agent=parser.get('praw', 'user_agent'),
                      username=parser.get('praw', 'username'))
 client = discord.Client()  # starts the discord client.
+
+def checkValidMeme(meme):
+    if('jpg' in meme or 'png' in meme or 'gif' in meme and meme not in lastTenPosts):
+        return True
+    else:
+        return False
 
 @client.event
 async def on_ready():  # method expected by client. This runs once when connected
@@ -26,15 +33,21 @@ async def on_message(message):  # event that happens per any message.
 
     if m_content.startswith('!rant'):
         await message.channel.send('Buckle up kiddos, its going to get political')
-    if ' voting ' in m_content or ' vote ' in m_content:
+    if 'voting' in m_content or 'vote' in m_content:
         await message.add_reaction('<:soldiersam:774092343120887808>')
-    if ' tyranny ' in m_content or ' tyrannical ' in m_content or ' tyrant ' in m_content:
+    if 'tyranny' in m_content or 'tyrannical' in m_content or 'tyrant' in m_content:
         await message.add_reaction('<:nostep:773692274449580062>')
-    if ' tax' in m_content:
+    if 'tax ' in m_content:
         await message.add_reaction('<:friedweegs:774055169569587260>')
     if m_content.startswith('!kmeme'):
         subreddit = reddit.subreddit("libertarianmeme")
-        await message.channel.send(subreddit.random().url)
+        memeToSend = subreddit.random().url
+
+        while not checkValidMeme(memeToSend):
+            memeToSend = subreddit.random().url
+
+        lastTenPosts.append(memeToSend)
+        await message.channel.send(memeToSend)
     else:
         print()
 
